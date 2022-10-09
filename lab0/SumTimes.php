7f06eed82081f7de849aa78442ea8ff4268be00e
Volodya <vpollikarpov@mail.ru>
2022-10-09 23:01:16 +0300

    working

--- lab0/SumTimes.php
+++ lab0/SumTimes.php
@@ -1,28 +1,45 @@
 <?php
-function SumtTimes($str1,$str2):string
+function SumtTimes(string $str1,string $str2):string
 { 
-       $chisla1 = array(10);
-       $chisla2 = array(10);
-       $result = array(20);
+    $time1 = array(10);
+    $time2 = array(10);
+    $result = array(20);
+        
+    $time1 = explode(':',$str1);
+    $time2 = explode(':',$str2);
        
-       
-       
-       for($i = 0;$i < 8;$i++)
-       {
-          explode($str1,':');
-           echo $str1;
+    for($i = 0;$i<count($time1);$i++){
+              
+           $result[$i] = $time1[$i] + $time2[$i];
+    }
 
-         
-       }  
+    if($result[2] >= 60)
+    {    
+         $temp = 60 - $time1[2];
+         $temp = $time2[2] - $temp;
+         $result[2] = $temp;
+         $result[1]++; 
+    }
+
+    if($result[1] >= 60)
+    {
+         $temp = 60 - $time1[1];
+         $temp = $time2[1] - $temp;
+         $result[1] = $temp;
+         $result[0]++; 
+    }
+
+    if($result[0] >= 24)
+    {
+         $temp =  24 - $time1[0];
+         $temp = $time2[0] - $temp;
+         $result[0] = $temp; 
+    }
+
        
-return $str1;
+   return $result[0] . ':' . $result[1] . ':' . $result[2];
 }
 
-
-
-$str1 = "10:20:30";
-$str2 = "18:40:30";
-SumtTimes($str1,$str2);
-
-
-?>
+$str1 = '20:20:50';
+$str2 = '10:20:30';
+echo SumtTimes($str1,$str2);
