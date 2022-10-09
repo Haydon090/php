d047d52554f52fa5b9f0d2e4a8e0c631dbb93cee
Volodya <vpollikarpov@mail.ru>
2022-10-09 23:18:41 +0300

    Sumtimes workiing

--- lab0/SumTimes.php
+++ lab0/SumTimes.php
@@ -1,45 +1,41 @@
 <?php
-function SumtTimes(string $str1,string $str2):string
-{ 
+function SumtTimes(string $str1, string $str2): string
+{
     $time1 = array(10);
     $time2 = array(10);
     $result = array(20);
-        
-    $time1 = explode(':',$str1);
-    $time2 = explode(':',$str2);
-       
-    for($i = 0;$i<count($time1);$i++){
-              
-           $result[$i] = $time1[$i] + $time2[$i];
+
+    $time1 = explode(':', $str1);
+    $time2 = explode(':', $str2);
+
+    for ($i = 0; $i < count($time1); $i++) {
+
+        $result[$i] = $time1[$i] + $time2[$i];
     }
 
-    if($result[2] >= 60)
-    {    
-         $temp = 60 - $time1[2];
-         $temp = $time2[2] - $temp;
-         $result[2] = $temp;
-         $result[1]++; 
+    if ($result[2] >= 60) {
+        $temp = 60 - $time1[2];
+        $temp = $time2[2] - $temp;
+        $result[2] = $temp;
+        $result[1]++;
     }
 
-    if($result[1] >= 60)
-    {
-         $temp = 60 - $time1[1];
-         $temp = $time2[1] - $temp;
-         $result[1] = $temp;
-         $result[0]++; 
+    if ($result[1] >= 60) {
+        $temp = 60 - $time1[1];
+        $temp = $time2[1] - $temp;
+        $result[1] = $temp;
+        $result[0]++;
     }
 
-    if($result[0] >= 24)
-    {
-         $temp =  24 - $time1[0];
-         $temp = $time2[0] - $temp;
-         $result[0] = $temp; 
+    if ($result[0] >= 24) {
+        $temp =  24 - $time1[0];
+        $temp = $time2[0] - $temp;
+        $result[0] = $temp;
     }
 
-       
-   return $result[0] . ':' . $result[1] . ':' . $result[2];
+    return $result[0] . ':' . $result[1] . ':' . $result[2];
 }
 
 $str1 = '20:20:50';
 $str2 = '10:20:30';
-echo SumtTimes($str1,$str2);
+echo SumtTimes($str1, $str2);
--- lab0/calculator.php
+++ lab0/calculator.php
@@ -1,20 +1,26 @@
 <?php
-function calculator($str):string
-{ 
-    $chisla = array(20);
-    $znaki = array(10);
+function calculator($str): string
+{
+    $numbers = array(20);
+    $operations = array(10);
     $str1 = '';
-    for($i = 0; $i < strlen($str);$i++)
-    {  
-        if($str[$i] == "+" || $str[$i] == "-")
-        {
-            $znaki[$i] = $str[$i];
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] === '+' || $str[$i] === '-') {
+            $operations[$i] = $str[$i];
         }
+    }
+    if (count($operations) > 4) {
+        return 'Ошибка ввода';
+    }
+    $correctInput = '0123456789+-';
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (!strpos($correctInput, $str[$i])) {
+            return 'Ошибка ввода';
+        }
+    }
 
-    } 
-  return $str;
+
+    return $str;
 }
-$str = "12 + 3 - 1";
+$str = "12+3-1";
 calculator($str);
-
-?>
