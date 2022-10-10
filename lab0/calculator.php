fa9c09b3c1d3c515a595684b1d4103b7b623425e
Volodya <vpollikarpov@mail.ru>
2022-10-10 20:57:13 +0300

    calculator working

--- lab0/calculator.php
+++ lab0/calculator.php
@@ -1,21 +1,23 @@
 <?php
-  function calculator(string $str) : string{
+function calculator(string $str): string
+{
     $operations = [];
     $numbers = [];
-    
 
-    for($i = 0; $i < strlen($str); $i++){
-      if($str[$i] === '+' || $str[$i] === '-'){
-        $operations[$i] = $str[$i];
-        
-      }
+
+    for ($i = 0; $i < strlen($str); $i++)
+    {
+        if ($str[$i] === '+' || $str[$i] === '-') {
+            $operations[$i] = $str[$i];
+        }
     }
-    
+
     $error = '0123456789+-';
-    for($i = 0; $i < strlen($str); $i++){
-      if(!strpos($error, $str[$i])){
-        return 'Ошибка ввода';
-     }
+    for ($i = 0; $i < strlen($str); $i++) 
+    {
+        if (!strpos($error, $str[$i])) {
+            return 'Ошибка ввода';
+        }
     }
 
     $numbers = explode('+', $str);
@@ -23,27 +25,25 @@
     $temp = '';
     $sum = 0;
 
-    foreach($numbers as $num){
-      $minusPos = strpos($num, '-');
-      if($minusPos){
-        $numbersWithMinus = explode('-', $num);
-        foreach($numbersWithMinus as $keyMinus => $numMinus){
-          if($keyMinus === 0){
-            $temp = $numMinus;
+    foreach ($numbers as $num) {
+        $minusPos = strpos($num, '-');
+        if ($minusPos) {
+            $numbersWithMinus = explode('-', $num);
+            foreach ($numbersWithMinus as $keyMinus => $numMinus) {
+                if ($keyMinus === 0) {
+                    $temp = $numMinus;
+                    $numberOfOperations++;
+                } else {
+                    $temp = $temp - (int)$numMinus;
+                    $numberOfOperations++;
+                }
+            }
+            $sum += $temp;
+        } else {
+            $sum += $num;
             $numberOfOperations++;
-          }
-          else{
-            $temp = $temp - (int)$numMinus;
-            $numberOfOperations++;
-          }
         }
-        $sum += $temp;
-      }
-      else{
-        $sum += $num;
-        $numberOfOperations++;
-      }
     }
     return $sum;
-  }
-  echo calculator('1+4-3');
+}
+echo calculator('1+4-3');
